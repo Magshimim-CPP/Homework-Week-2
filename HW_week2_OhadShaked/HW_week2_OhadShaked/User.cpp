1513aa177ccfcc325f1f1fb20f8d5c9cc2b52bb4
Ohad Shaked <ohad.saked@gmail.com>
2022-11-14 13:10:21 +0200

    completed User.h and User.cpp - tested using test2User.cpp

---
+++ HW_week2_OhadShaked/HW_week2_OhadShaked/User.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include "User.h"
+
+#define ZERO 0
+
+void User::init(unsigned int id, string username, unsigned int age)
+{
+	this->id = id;
+	this->name = username;
+	this->age = age;
+	this->devicesList = new DevicesList;
+	this->devicesList->init();
+}
+
+void User::clear()
+{
+	devicesList->clear();
+	delete(devicesList);
+	age = ZERO;
+	id = ZERO;
+	name = "";
+}
+
+unsigned int User::getID() const
+{
+	return id;
+}
+
+string User::getUserName() const
+{
+	return name;
+}
+
+unsigned int User::getAge() const
+{
+	return age;
+}
+
+DevicesList& User::getDevices() const
+{
+	return *devicesList;
+}
+
+void User::addDevice(Device newDevice)
+{
+	devicesList->add(newDevice);
+}
+
+bool User::checkIfDevicesAreOn() const
+{
+	DeviceNode* temp = devicesList->get_first();
+
+	while (temp)
+	{
+		if (!(temp->get_data().isActive()))
+		{
+			return false;
+		}
+		temp = temp->get_next();
+	}
+
+	return true;
+}
---
+++ HW_week2_OhadShaked/HW_week2_OhadShaked/User.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+#include "DeviceList.h"
+
+using namespace std;
+
+class User
+{
+private:
+	unsigned int id;
+	string name;
+	unsigned int age;
+	DevicesList* devicesList;
+
+public:
+	void init(unsigned int id, string username, unsigned int age);
+	void clear();
+	unsigned int getID() const;
+	string getUserName() const;
+	unsigned int getAge() const;
+	DevicesList& getDevices() const;
+	void addDevice(Device newDevice);
+	bool checkIfDevicesAreOn() const;
+
+};
