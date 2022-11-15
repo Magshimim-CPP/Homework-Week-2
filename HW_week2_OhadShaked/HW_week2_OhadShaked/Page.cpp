e0849b32cbc011ebb2cb0f92c7184ea53f62db6f
Ohad Shaked <ohad.saked@gmail.com>
2022-11-15 19:21:55 +0200

    made a few changes and added comments to Page.h and Page.cpp files.

--- HW_week2_OhadShaked/HW_week2_OhadShaked/Page.cpp
+++ HW_week2_OhadShaked/HW_week2_OhadShaked/Page.cpp
@@ -1,31 +1,64 @@
 #include "Page.h"
 
+//setting define constant variable for empty string content.
+#define EMPTY ""
+
+/*
+Initialization function for the page class.
+Input: none.
+Output: none.
+*/
 void Page::init()
 {
-	status = "";
-	posts = "";
+	status = EMPTY;
+	posts = EMPTY;
 }
 
+/*
+Const 'get' function that returns the page's posts.
+Input: none.
+Output: the page's posts string (string posts).
+*/
 string Page::getPosts() const
 {
 	return posts;
 }
 
+/*
+Const 'get' function that returns the page's status.
+Input: none.
+Output: the page's status string (string status).
+*/
 string Page::getStatus() const
 {
 	return status;
 }
 
+/*
+Function reset's the page's posts string.
+input: none.
+Output: none.
+*/
 void Page::clearPage()
 {
-	posts = "";
+	posts = EMPTY;
 }
 
+/*
+Function sets the page's status to a given string.
+Input: string status
+Output: none
+*/
 void Page::setStatus(string status)
 {
 	this->status = status;
 }
 
+/*
+Function adds a given string to the page's 'posts' string in a new line.
+Input: string new_line
+Output: none.
+*/
 void Page::addLineToPosts(string new_line)
 {
 	posts += new_line + "\n";
--- HW_week2_OhadShaked/HW_week2_OhadShaked/Page.h
+++ HW_week2_OhadShaked/HW_week2_OhadShaked/Page.h
@@ -4,17 +4,20 @@
 
 using namespace std;
 
+//Page class
 class Page
 {
+//setting private class variables.
 private:
-	string status;
-	string posts;
+	string status; //the user's page status.
+	string posts; //the user's posts at the page.
 
+//setting public functions for the page class.
 public:
-	void init();
-	string getPosts() const;
-	string getStatus() const;
-	void clearPage();
-	void setStatus(string status);
-	void addLineToPosts(string new_line);
+	void init(); //Initialization function for the page class.
+	string getPosts() const; //const 'get' function that returns the page's posts.
+	string getStatus() const; //const 'get' function that returns the page's status.
+	void clearPage(); //function to reset the page's posts string.
+	void setStatus(string status); //function sets the page's status to a given string.
+	void addLineToPosts(string new_line); //function adds a given string to the page's 'posts' string in a new line.
 };
