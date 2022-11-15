a5e5c0900be2f5cd3a9198e2e8b3ec7ac9eced4c
Ohad Shaked <ohad.saked@gmail.com>
2022-11-15 18:59:35 +0200

    added comments to User.h and User.cpp files.

--- HW_week2_OhadShaked/HW_week2_OhadShaked/User.cpp
+++ HW_week2_OhadShaked/HW_week2_OhadShaked/User.cpp
@@ -3,61 +3,105 @@
 
 #define ZERO 0
 
+/*
+Initialization function for the user class.
+Input: unsigned int id, string name, unsigned int age
+Output: none.
+*/
 void User::init(unsigned int id, string name, unsigned int age)
 {
 	this->id = id;
 	this->name = name;
 	this->age = age;
-	this->devicesList = new DevicesList;
-	this->devicesList->init();
+	this->deviceList = new DevicesList;
+	this->deviceList->init();
 }
 
+/*
+Function to reset and clear all of the user's data.
+Input: none.
+Output: none.
+*/
 void User::clear()
 {
-	devicesList->clear();
-	delete(devicesList);
+	deviceList->clear();
+	delete(deviceList);
 	age = ZERO;
 	id = ZERO;
 	name = "";
 }
 
+/*
+Const 'get' function that returns the user's id.
+Input: none
+Output: the user's id (unsigned int id).
+*/
 unsigned int User::getID() const
 {
 	return id;
 }
 
+/*
+Const 'get' function that returns the user's name.
+Input: none
+Output: the user's name (string name).
+*/
 string User::getUserName() const
 {
 	return name;
 }
 
+/*
+Const 'get' function that returns the user's age.
+Input: none
+Output: the user's age (unsigned int age).
+*/
 unsigned int User::getAge() const
 {
 	return age;
 }
 
+/*
+Const 'get' function that returns a reference to the user's device list.
+Input: none
+Output: a reference to the user's device list (DevicesList& deviceList).
+*/
 DevicesList& User::getDevices() const
 {
-	return *devicesList;
+	return *deviceList;
 }
 
+/*
+Function adds a given new device object to the user's device list.
+Input: Device newDevice
+Output: none.
+*/
 void User::addDevice(Device newDevice)
 {
-	devicesList->add(newDevice);
+	deviceList->add(newDevice);
 }
 
+/*
+Const function to return if all the user's devices are on (true - yes / false - no)
+Input: none.
+Output: true or false based on the result (true - yes / false - no)
+*/
 bool User::checkIfDevicesAreOn() const
 {
-	DeviceNode* temp = devicesList->get_first();
+	//creating a temp node for the loop checking proccess.
+	DeviceNode* dev = deviceList->get_first();
 
-	while (temp)
+	//looping as long as a next node exists (not null).
+	while (dev)
 	{
-		if (!(temp->get_data().isActive()))
+		//if the currently looped node isn't active, then at least one device is off, returning false.
+		if (!(dev->get_data().isActive()))
 		{
 			return false;
 		}
-		temp = temp->get_next();
+		//moving to the user's next device node.
+		dev = dev->get_next();
 	}
-
+	//if we looped through all the device nodes and didn't return false, then all of the devices are on, returning true.
 	return true;
 }
--- HW_week2_OhadShaked/HW_week2_OhadShaked/User.h
+++ HW_week2_OhadShaked/HW_week2_OhadShaked/User.h
@@ -4,21 +4,24 @@
 
 using namespace std;
 
+//User class
 class User
 {
+//setting private class variables.
 private:
-	unsigned int id;
-	string name;
-	unsigned int age;
-	DevicesList* devicesList;
+	unsigned int id; //User id.
+	string name; //The user's name.
+	unsigned int age; //User's age.
+	DevicesList* deviceList; //A list of the user's devices.
 
+//setting public functions for the user class.
 public:
-	void init(unsigned int id, string username, unsigned int age);
-	void clear();
-	unsigned int getID() const;
-	string getUserName() const;
-	unsigned int getAge() const;
-	DevicesList& getDevices() const;
-	void addDevice(Device newDevice);
-	bool checkIfDevicesAreOn() const;
+	void init(unsigned int id, string username, unsigned int age); //Initialization function for the user class.
+	void clear(); //function to reset and clear all of the user's data.
+	unsigned int getID() const; //const 'get' function that returns the user's id.
+	string getUserName() const; //const 'get' function that returns the user's name.
+	unsigned int getAge() const; //const 'get' function that returns the user's age.
+	DevicesList& getDevices() const; //const 'get' function that returns a reference to the user's device list.
+	void addDevice(Device newDevice); //function adds a given new device object to the user's device list.
+	bool checkIfDevicesAreOn() const; //const function to return if all the user's devices are on (true - yes / false - no)
 };
